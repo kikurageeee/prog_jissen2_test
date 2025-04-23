b8ce5790c5c58adf97c3484202adcda2fc34d8cf
kikurageeee <mentaikokko6778@gmail.com>
2025-04-23 10:03:22 +0900

    First Commit

---
+++ app/src/test/java/jp/ac/gifu_u/suzuki1233033089/myapplication/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package jp.ac.gifu_u.suzuki1233033089.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
